4b86236865a381fea2b38c1b8637c84522d27e54
Md. Almas Ali <almaspr3@gmail.com>
2022-08-05 03:22:35 +0600

    Added Docs.

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ if __name__ == '__main__':
 		for i in range(1000):
 			main()
 			print(f'Done {i}')
-	except keyboardinterrupt as e:
+	except keyboardInterrupt as e:
 		os.system('git push -u origin master')
 
