c1485bde9c27145cbd5adaaadae0c4921c68e66f
Md. Almas Ali <almaspr3@gmail.com>
2022-08-05 03:09:27 +0600

    Added Docs.

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import glob
+
+
+def main():
+	file = 'README.md'
+	with open(file, 'a') as docs:
+		docs.write('# This is the documentation of this project.')
+
+	os.system('git add .')
+	os.system('git commit -m "Added Docs." ')
+
+	os.remove(file)
+
+	os.system('git add .')
+	os.system('git commit -m "Removed Docs." ')
+
+
+
+if __name__ == '__main__':
+	main()
