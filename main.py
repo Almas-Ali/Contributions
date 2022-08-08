4f7a21a1c934841320107109321d5613f617ee44
Md. Almas Ali <almaspr3@gmail.com>
2022-08-09 00:19:52 +0600

    Removed Contribution file.

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def worker(filename:str, filecontent:str, adderstring:str, removerstring:str):
 	# Removing it from system.
 	os.remove(filename)
 
-	
-	
+	sec = dt.now().strftime()
+
 	# Adding remove descriptions and commiting it.
 	os.system('git add .')
 	os.system(f'git commit -m "{removerstring}" ')
